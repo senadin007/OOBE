d63de41288ac68babaf29d7d3231b73ded420c3d
Aid Fatušić <aid.fatusic@secomind.com>
2026-02-05 13:30:50 +0100

    Update package files for dependencies
    - Added Bootstrap and React-Bootstrap.
    - Deleted unnecessary content.
    - Updated Vite configuration for React.
    - Updated TypeScript configuration for React.
    - Updated boilerplate code for React application.
    
    Signed-off-by: Aid Fatušić <aid.fatusic@secomind.com>

--- oobe-apps/smart-building-app/src/App.tsx
+++ oobe-apps/smart-building-app/src/App.tsx
@@ -1,35 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Container, Spinner } from "react-bootstrap";
 
-function App() {
-  const [count, setCount] = useState(0);
+export type AppProps = {
+  astarteUrl: URL;
+  realm: string;
+  deviceId: string;
+  token: string;
+};
+
+const App = ({ astarteUrl, realm, deviceId, token }: AppProps) => {
+  const [dataFetching, setDataFetching] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container fluid className="p-4">
+      {dataFetching ? (
+        <div className="p-2 p-md-4 text-center">
+          <Spinner />
+        </div>
+      ) : (
+        "NAZIV_APLIKACIJE"
+        // TODO: implement components
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
