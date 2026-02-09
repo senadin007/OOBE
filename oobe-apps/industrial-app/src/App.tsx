bbcd06693e6a333b23862be76209f2352237e8e5
Senadin Omerovic <senadin.omerovic@secomind.com>
2026-02-09 14:21:14 +0100

    Create initial app architecture with boilerplate code to build the app
    And basic implementations of lifecycle functions (mount/unmount).

--- oobe-apps/industrial-app/src/App.tsx
+++ oobe-apps/industrial-app/src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { useState } from "react";
+import { Container, Spinner } from "react-bootstrap";
 
-function App() {
-  return <h1>Industrial App</h1>;
-}
+export type AppProps = {
+  astarteUrl: URL;
+  realm: string;
+  deviceId: string;
+  token: string;
+};
+
+const App = ({ astarteUrl, realm, deviceId, token }: AppProps) => {
+  const [dataFetching, setDataFetching] = useState(false);
+
+  return (
+    <Container fluid className="p-4">
+      {dataFetching ? (
+        <div className="p-2 p-md-4 text-center">
+          <Spinner />
+        </div>
+      ) : (
+        <h1>Industrial App</h1>
+        // TODO: implement components
+      )}
+    </Container>
+  );
+};
 
 export default App;
